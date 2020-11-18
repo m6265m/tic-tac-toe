51531b21d730a2bce976f792f271c9bf61070909
Maha <tausif.maha@gmail.com>
2020-11-18 12:49:08 +0500

    with time travel commit

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,81 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Board from "./Board";
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 function Game(props) {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXisNext] = useState(true);
+    const winner = calculateWinner(history[stepNumber]);
+
+    const handleClick = i => {
+        const timeInHistory = history.slice(0, stepNumber + 1);
+        const current = timeInHistory[stepNumber];
+        const squares = [...current];
+        // If user click an occupied square or if game is won, return
+        if (winner || squares[i]) return;
+        // Put an X or an O in the clicked square
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory([...timeInHistory, squares]);
+        setStepNumber(timeInHistory.length);
+        setXisNext(!xIsNext);
+    }
+
+
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXisNext(step % 2 === 0)
+    }
+
+    const renderMoves = () => (
+        history.map((_step, move) => {
+            const destination = move ? `Go to move#${move}` : 'Go to start';
+            return (
+                <li key={move}>
+                    <button onClick={() => jumpTo(move)}>{destination}</button>
+                </li>
+            )
+        })
+    )
+
+
+    let status;
+
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                <Board
+                    value={history[stepNumber]}
+                    onClick={(i) => handleClick(i)}/>
             </div>
             <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
+                <div>{status} </div>
+                <ol>{renderMoves()}</ol>
             </div>
         </div>
     );
